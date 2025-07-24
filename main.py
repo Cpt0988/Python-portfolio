86040e279afc8f69a91ddf32c801c8437a30d007
Chuck Taylor <cpt2948@gmail.com>
2025-07-23 21:50:43 -0400

    Added description, image & links

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st
+import pandas
 
 
 st.set_page_config(layout= "wide")
 col1, col2 = st.columns(2)
+
 
 with col1:
     st.image("images/photo.png", width= 300)
@@ -19,3 +21,21 @@ statement="""
 Below you can find some the apps I have built in Python. Feel free to contact me!
 """
 st.info(statement)
+
+col3,empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source code]({row['url']})")
+        
+        
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/"+row["image"])
+        st.write(f"[Source code]({row['url']})")
