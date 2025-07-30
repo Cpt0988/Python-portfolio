95738c8a18d156ed63b9f2b03e71a3c420dd115c
Chuck Taylor <cpt2948@gmail.com>
2025-07-29 22:28:25 -0400

    Setup Send Email

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
 st.header("Contact me")
+
+with st.form(key = "email form"):
+    user_email = st.text_input("Your Email Address")
+    message = st.text_area("Your message")
+    button= st.form_submit_button("Submit")
+    if button:
+        message = message + user_email
---
+++ send_email.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+username = "c.p.taylor936@gmail.com"
+password= "bajj nksd pfad sene"
+
+receiver = "c.p.taylor936@gmail.com"
+context = ssl.create_default_context()
+
+message="""\
+Subject: Test
+Thank you.
+"""
+
+with smtplib.SMTP_SSL(host,port, context = context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
